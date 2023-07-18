29759710bac4c7d60f0fe8a3c91589c95faa3768
VamshiLTI <139456120+VamshiLTI@users.noreply.github.com>
2023-07-18 11:36:55 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+// Create the server
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, There!');
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
